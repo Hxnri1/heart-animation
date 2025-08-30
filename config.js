18779e2683b02274e6e5995096e7d97a9f42ddf4
Hxnri1 <135207002+Hxnri1@users.noreply.github.com>
2025-08-30 16:56:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
-
 var config = {
-    
-    lines: [
-  "Meine liebste Kim, du bist mein Sternenschein,",
-  "Wie dieser Baum wächst, wächst mein Herz für dich – ganz rein.",
-  "Und wenn ich dich erblicke, wächst auch mein Glied,",
-  "Bei dir verschwinden Sorgen, alles Schwere entflieht.",
-  "Unendlich tief ist meine Liebe, das spürst du in mir,",
-  "Und ja, ich gestehe: auch deine Füße bedeuten Glück mir."
-   
-    date: "2024-02-24 00:00:00",
-   
-    names: ["Kim", "Brieuc"]
+  lines: [
+    "Meine liebste Kim, du bist mein Sternenschein,",
+    "Wie dieser Baum wächst, wächst mein Herz für dich – ganz rein.",
+    "Und wenn ich dich erblicke, wächst auch mein Glied,",
+    "Bei dir verschwinden Sorgen, alles Schwere entflieht.",
+    "Unendlich tief ist meine Liebe, das spürst du in mir,",
+    "Und ja, ich gestehe: auch deine Füße bedeuten Glück mir."
+  ],
+  date: "2024-02-24 00:00:00",
+  names: ["Kim", "Brieuc"]
 }
